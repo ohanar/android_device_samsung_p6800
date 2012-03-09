e3e1ee6d5e497d5c47e3fd0735285042d8e80a6e
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-03-08 16:14:41 -0800

    remove old galaxys2 remnants

--- cm.mk
+++ cm.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := SGS2
+PRODUCT_RELEASE_NAME := TAB7.7WIFI
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
