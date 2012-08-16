0422d42b08238f39bb3659f2cbc5c4d8657f2d7e
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-08-15 18:42:22 -0700

    cleanup/reorganize

--- cm.mk
+++ cm.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/p6800_base.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Samsung
--- device.mk
+++ device.mk
@@ -16,19 +16,21 @@
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/samsung/smdk4210-tab/smdk4210-tab_base.mk)
-$(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/smdk4210-tab_base.mk)
-$(call inherit-product-if-exists, vendor/samsung/p6800/p6800_base.mk)
+$(call inherit-product, device/samsung/smdk4210-tab/device.mk)
+$(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/p6800/vendor.mk)
 
 # the Tab 7.7 has a xlarge hpdi screen
 PRODUCT_AAPT_CONFIG := xlarge hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_LOCALES += hdpi
 
-# needs to be overwritten to get android to scale appropriately
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=160
-
-# These are the hardware-specific features
+# GSM
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libargs=-d /dev/ttyS0 \
+    ro.telephony.ril_class=Smdk4210RIL
