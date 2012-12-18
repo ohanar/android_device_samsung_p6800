32ed7a7ca9896aaba44e2c2b3c07495d2b703ad4
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-12-17 16:17:50 -0800

    move audio confs out of common tree

--- device.mk
+++ device.mk
@@ -13,19 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, device/samsung/smdk4210-tab/device.mk)
+$(call inherit-product-if-exists, vendor/samsung/p6800/vendor.mk)
+
 # Set preferred size for assets
 PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Audio
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/etc/tinyalsa-audio.xml:system/etc/tinyalsa-audio.xml \
+	$(LOCAL_PATH)/configs/etc/audio_policy.conf:system/etc/audio_policy.conf
+
+# International variants have proximity sensor
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml 
+
+# Radio
+$(call inherit-product, build/target/product/telephony.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/samsung/smdk4210-tab/device.mk)
-$(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/vendor.mk)
-$(call inherit-product-if-exists, vendor/samsung/p6800/vendor.mk)
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 PRODUCT_PACKAGES += \
-    SamsungServiceMode
+	SamsungServiceMode \
+	VoiceDialer
